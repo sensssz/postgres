4f4010795301aae1d4e629fdf2ba91e69b2ff47e
sensssz <hjmsens@gmail.com>
2016-02-13 12:13:11 -0500

    Add log around function

--- src/backend/vprofiler/trace_tool.cc
+++ src/backend/vprofiler/trace_tool.cc
@@ -192,11 +192,13 @@ void TRACE_FUNCTION_START() {
 
 void TRACE_FUNCTION_END() {
 #ifdef MONITOR
+    TraceTool::get_instance()->log_file << "Function starts" << endl;
     if (TraceTool::should_monitor()) {
         clock_gettime(CLOCK_REALTIME, &function_end);
         long duration = TraceTool::difftime(function_start, function_end);
         TraceTool::get_instance()->add_record(0, duration);
     }
+    TraceTool::get_instance()->log_file << "Function ends" << endl;
 #endif
 }
 
